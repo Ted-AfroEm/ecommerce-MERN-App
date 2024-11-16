576d5bd7bd6b5909789f80ac94d1cd6a7e91d38d
Tedros Kahsu <tedkahcom@gmail.com>
2024-11-16 12:48:27 +0300

    Connected to DB succefully

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,17 +1,17 @@
-import express from 'express';
-import cors from 'cors';
-// import connectDB from 'config/mongodb';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb";
 
 const app = express();
-const port  = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
+connectDB();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
 
-
-app.get('/',(req, res)=> {
-    res.send("API Working")
-})
-
-app.listen(port, ()=> console.log(`Server started on PORT: `+ port))
+app.listen(port, () => console.log(`Server started on PORT: ` + port));
