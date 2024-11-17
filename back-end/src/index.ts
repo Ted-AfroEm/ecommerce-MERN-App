a0bd2472a132fc4ef68f0b57958d5c51eeb08888
Tedros Kahsu <tedkahcom@gmail.com>
2024-11-17 15:25:53 +0300

    Add product model and connect to cloudnary

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb";
+import connectcloudinary from "./config/cloudnary";
 
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
+connectcloudinary();
 
 app.use(express.json());
 app.use(cors());
